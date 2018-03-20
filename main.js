210f425fad6a34420d4abc252a8527d230deb808
Enrico Hoffmann <dasrick@gmail.com>
2018-03-20 15:14:16 +0100

    append setup with morgan

--- main.js
+++ main.js
@@ -5,10 +5,14 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const favicon = require('serve-favicon');
-var path = require('path');
+const path = require('path');
+const logger = require('morgan');
 
 // create
 const app = express();
+const hour = 3600000;
+const day = hour * 24;
+const week = day * 7;
 
 // set up
 app.set('port', process.env.PORT || 3001);
@@ -17,10 +21,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors());
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, 'public'), {maxAge: week}));
+app.use(logger('dev'));
 
 // Public route
 app.get('/api/deals/public', function (req, res) {
-    var deals = [
+    const deals = [
         // Array of public deals here
     ];
     res.json(deals);
@@ -28,7 +34,7 @@ app.get('/api/deals/public', function (req, res) {
 
 // Private route
 app.get('/api/deals/private', function (req, res) {
-    var deals = [
+    const deals = [
         // Array of Private Deals here
     ];
     res.json(deals);
